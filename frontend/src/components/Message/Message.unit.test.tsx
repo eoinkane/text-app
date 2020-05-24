41189a8f5a22bb4cca5b582e12cc732d0ccc70e4
eoinkane <eoinkane23@gmail.com>
2020-05-24 13:25:04 +0100

    Fix Test

--- frontend/src/components/Message/Message.unit.test.tsx
+++ frontend/src/components/Message/Message.unit.test.tsx
@@ -7,7 +7,7 @@ describe("Message", () => {
     const text =
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu consequat dui. Vivamus arcu tellus, tristique at purus ut, sagittis volutpat justo. Phasellus imperdiet ex. ";
     const message = shallow(<Message text={text} />);
-    expect(Message).toMatchSnapshot();
+    expect(message).toMatchSnapshot();
   });
 
   it("Renders with the correct text", () => {
@@ -15,7 +15,7 @@ describe("Message", () => {
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu consequat dui. Vivamus arcu tellus, tristique at purus ut, sagittis volutpat justo. Phasellus imperdiet ex. ";
     const message = shallow(<Message text={text} />);
 
-    expect(Message).toMatchSnapshot();
+    expect(message).toMatchSnapshot();
     expect(message.text()).toBe(text);
   });
 });
