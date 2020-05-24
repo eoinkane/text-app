1bd95ae66183d1090059ae9c19d55bac5f05d016
eoinkane <eoinkane23@gmail.com>
2020-05-24 17:27:33 +0100

    Message Data Model

---
+++ frontend/src/models/Message.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosResponse } from "axios";
+
+interface MessageProps {
+  id?: number;
+  sender?: number;
+  message?: string;
+  conversationsId?: number;
+}
+
+type valueof<T> = T[keyof T];
+
+export default class Message {
+  constructor(private data: MessageProps) {}
+
+  get<K extends keyof MessageProps>(propName: K): valueof<MessageProps> {
+    return this.data[propName];
+  }
+
+  set(update: MessageProps): void {
+    Object.assign(this.data, update);
+  }
+  fetch(): void {
+    // axios
+    //   .get(`http://localhost:3000/users/${this.get("id")}`)
+    //   .then((response: AxiosResponse): void => {
+    //     this.set(response.data);
+    //   });
+  }
+
+  save(): void {
+    // const id = this.get("id");
+    // if (id) {
+    //   axios.put(`http://localhost:3000/users/${id}`, this.data);
+    // } else {
+    //   axios.post(`http://localhost:3000/users`, this.data);
+    // }
+  }
+}
