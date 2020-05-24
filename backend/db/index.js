8f96e2fb240c01988db10acf682da666332805aa
eoinkane <eoinkane23@gmail.com>
2020-05-25 00:24:01 +0100

    Added Restrictions of source

--- backend/db/index.js
+++ backend/db/index.js
@@ -4,6 +4,14 @@ const router = jsonServer.router("./db/db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use((req, res, next) => {
+  if (req.get("origin") === "flask-server") {
+    // add your authorization logic here
+    next(); // continue to JSON Server router
+  } else {
+    res.sendStatus(401);
+  }
+});
 server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
