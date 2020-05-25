5594c89398bcd20df39c40cbd77dfa6dd1ee80bd
eoinkane <eoinkane23@gmail.com>
2020-05-25 14:31:56 +0100

    Added a intialisation of db.json file

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,9 +1,51 @@
 const jsonServer = require("json-server");
+const fs = require("fs");
 const path = require("path");
 const axios = require("axios");
 const auth = require("basic-auth");
+const dbFilePath = path.join(__dirname, "db.json");
+
+const jsonDBserverStructure = {
+  users: [
+    {
+      id: 1,
+      userName: "admin",
+      firstName: "Admin",
+      lastName: "User",
+    },
+  ],
+  conversations: [],
+  messages: [],
+};
+
+try {
+  if (!fs.existsSync(dbFilePath)) {
+    const jsonDBserverStructureString = JSON.stringify(
+      jsonDBserverStructure,
+      null,
+      4
+    );
+
+    fs.writeFile(dbFilePath, jsonDBserverStructureString, "utf8", (err) => {
+      if (err) {
+        console.log(
+          "An error occured while writing JSON Object to JSON DB File."
+        );
+        return console.log(err);
+      }
+
+      console.log("JSON DB file has been saved.");
+    });
+  } else {
+    console.log("JSON DB file exists");
+  }
+} catch (err) {
+  console.log(err);
+  throw err;
+}
+
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+const router = jsonServer.router(dbFilePath);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
