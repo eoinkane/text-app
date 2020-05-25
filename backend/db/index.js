e21b37ee8e0127338f8abb604b3e217317febef0
eoinkane <eoinkane23@gmail.com>
2020-05-25 13:07:56 +0100

    JSON Server as Node file instead of JSON

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,11 +1,22 @@
 const jsonServer = require("json-server");
+const path = require("path");
+const axios = require("axios");
+const auth = require("basic-auth");
 const server = jsonServer.create();
-const router = jsonServer.router("./db/db.json");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use((req, res, next) => {
-  if (req.get("origin") === "flask-server") {
+server.use(async (req, res, next) => {
+  if (req.headers["authorization"] === null) res.sendStatus(400);
+  const user = auth.parse(req.headers["authorization"]);
+  const r = await axios.get("http://localhost:5000/verify_user", {
+    auth: {
+      username: user.name,
+      password: user.pass,
+    },
+  });
+  if (r.status === 200) {
     // add your authorization logic here
     next(); // continue to JSON Server router
   } else {
