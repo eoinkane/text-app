bd2084908bc8045697bf01b1e7184ddf4ede1714
eoinkane <eoinkane23@gmail.com>
2021-02-12 14:57:56 +0000

    Stash commit

--- frontend/src/components/SignOn/SignOn.tsx
+++ frontend/src/components/SignOn/SignOn.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useRef } from 'react';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import TextBox from '../TextBox';
 
-const SignOn: React.FC = () => {
-  return <></>;
+interface SignOnBaseProps {}
+
+interface SignOnLoginProps extends SignOnBaseProps {
+  login: true;
+}
+
+interface SignOnSignUpProps extends SignOnBaseProps {
+  signup: true;
+}
+
+type SignOnProps = SignOnLoginProps | SignOnSignUpProps;
+
+const useStyles = makeStyles({
+  container: {
+    backgroundColor: "#f5f5f5",
+    maxWidth: "inherit",
+  },
+})
+
+const SignOn: React.FC<SignOnProps> = ({children, ...mode}) => {
+  const box1 = useRef<HTMLInputElement>(null);
+  const classes = useStyles();
+
+  const r = (() => {
+    if (mode.hasOwnProperty("login")) return ""
+    return undefined
+  })();
+
+  return (
+    <>
+      <TextBox id="Name" user product="Name" ref={box1}></TextBox>
+    </>
+  );
 };
 
 export default SignOn;
